0029d85e9b60c9f8be3b3950da0443f054962fe0
ilnazmen <ilnaz.cynopm@mail.ru>
2023-03-15 23:04:54 +0300

    Post work

--- src/components/books/books.vue
+++ src/components/books/books.vue
@@ -3,30 +3,30 @@
     <h1>Книги</h1>
     <form @submit.prevent="addBook">
       <div class="form-group">
-        <input type="text" class="form-control mt-3"  placeholder="Название книги">
-        <input type="text" class="form-control mt-3"  placeholder="Имя Автора">
-        <input type="text" class="form-control mt-3"  placeholder="Издатель">
-        <input class="form-control mt-3" type="file">
-        <date-pick class="mt-3"></date-pick>
-        <textarea placeholder="Описание книги" class="form-control mt-3" rows="3"></textarea>
+        <input type="text" class="form-control mt-3" v-model="name" placeholder="Название книги">
+        <input type="text" class="form-control mt-3" v-model="author" placeholder="Имя Автора">
+        <input type="text" class="form-control mt-3" v-model="publisher" placeholder="Издатель">
+        <input class="form-control mt-3" v-on:change="changeFile" id="photo" name="photo" type="file">
+        <date-pick v-model="release_date" class="mt-3"></date-pick>
+        <textarea placeholder="Описание книги" v-model="description" class="form-control mt-3" rows="3"></textarea>
         <select class="form-select mt-3" multiple aria-label="multiple select example" v-model="genre">
-          <option v-for="genre in genres" :value="genre.id">{{genre.title}}</option>
+          <option v-for="genre in genres" :value="genre.id">{{ genre.title }}</option>
         </select>
         <select class="form-select mt-3" aria-label="Default select example" v-model="status">
-          <option  v-for="status in statuses" :value="status.id">{{status.title}}</option>
+          <option v-for="status in statuses" :value="status.id">{{ status.title }}</option>
         </select>
-        <button type="submit" class="btn btn-primary mt-3">Добавить</button>
+        <button type="submit" @click.prevent="addBook" class="btn btn-primary mt-3">Добавить</button>
       </div>
     </form>
     <div class="row">
       <div class="col-4" v-for="book in books">
-      <div class="card mt-3" >
+        <div class="card mt-3">
           <router-link class="card-body" :to="{name: 'showBook', params: {bookId: book.id}}">
-            <h5 class="card-title">{{book.name}}</h5>
-            <p class="card-text">{{book.author}}</p>
+            <h5 class="card-title">{{ book.name }}</h5>
+            <p class="card-text">{{ book.author }}</p>
           </router-link>
-        <button type="button" class="btn btn-danger" @click="deleteBook(book.id)">Удалить</button>
-      </div>
+          <button type="button" class="btn btn-danger" @click="deleteBook(book.id)">Удалить</button>
+        </div>
       </div>
     </div>
     <div class="alert alert-danger" role="alert" v-if="errored">
@@ -41,6 +41,8 @@
 <script>
 import showBook from "@/components/books/showBook.vue";
 import DatePick from "@/components/books/date-pick.vue";
+import dayjs from "dayjs";
+
 
 export default {
   components: {DatePick},
@@ -49,7 +51,7 @@ export default {
       return showBook
     },
   },
-  data(){
+  data() {
     return {
       books: [],
       genres: [],
@@ -57,6 +59,13 @@ export default {
 
       genre: [],
       status: null,
+      name: null,
+      author: null,
+      publisher: null,
+      image: null,
+      description: null,
+      release_date: null,
+      formdata: null,
 
       errored: false,
       loading: true,
@@ -64,20 +73,27 @@ export default {
     }
   },
   methods: {
-    showGenre(){
+    changeFile(photo) {
+      this.image = photo.target.files[0]
+      // console.log(photo)
+      console.log(this.image)
+      console.log(this.genre)
+    },
+
+    showGenre() {
       axios.get('//localhost:8080/api/api/genres')
           .then(response => {
             this.genres = response.data
           })
-    },showStatus(){
+    }, showStatus() {
       axios.get('//localhost:8080/api/api/statuses')
           .then(response => {
-            this.statuses = response.data.filter((status)=>{
+            this.statuses = response.data.filter((status) => {
               return status.id === 1 || status.id === 4
             })
           })
     },
-    showAllBooks(){
+    showAllBooks() {
       axios.get('//localhost:8080/api/api/books')
           .then(response => {
             this.books = response.data.data
@@ -86,12 +102,12 @@ export default {
             console.log(error)
             this.errored = true
           })
-          .finally( () => {
+          .finally(() => {
             this.loading = false
           })
     },
-    deleteBook(id){
-      axios.post('//localhost:8080/api/api/books/' + id,{
+    deleteBook(id) {
+      axios.post('//localhost:8080/api/api/books/' + id, {
         _method: 'DELETE'
       })
           .then(response => {
@@ -102,20 +118,64 @@ export default {
             console.log(error)
             this.errored = true
           })
-          .finally( () => {
+          .finally(() => {
             this.loading = false
           })
     },
-    addBook(){
+    // bildAxios(){ if (
+    //     this.name != null,
+    //     this.author != null,
+    //     this.publisher != null,
+    //     this.description != null,
+    //     this.release_date != null,
+    //     this.status != null,
+    //     this.genre != null
+    // ) {
+    //       this.formdata = new FormData()
+    //       this.formdata.append('image', this.image),
+    //       this.formdata.append('name', this.name),
+    //       this.formdata.append('author', this.author),
+    //       this.formdata.append('publisher', this.publisher),
+    //       this.formdata.append('description', this.description),
+    //       this.formdata.append('release_date', this.release_date),
+    //       this.formdata.append('status', this.status),
+    //       this.formdata.append("genre_id", this.genre),
+    //       this.formdata.append('ImageUrl', '///')
+    //       console.log(this.formdata.get("genre_id"))
+    //
+    // }
+    // },
+    addBook() {
+      const data = new FormData()
+      data.append('image', this.image)
+      data.append('name', this.name)
+      data.append('author', this.author)
+      data.append('publisher', this.publisher)
+      data.append('description', this.description)
+      data.append('release_date', dayjs(this.release_date).format())
+      data.append('status_id', this.status)
+      this.genre.forEach((genre) => {
+        data.append("genre_id[]", genre)
+      })
+      data.append('ImageUrl', '///')
 
+      axios.post('//localhost:8080/api/api/books/', data)
+          .then(response => {
+            console.log(response)
+          })
+          .catch(error => {
+            console.log(error)
+            errored.value = true
+          })
     }
   },
-  mounted(){
+  mounted() {
     this.showAllBooks()
     this.showGenre()
     this.showStatus()
   }
 }
+
 </script>
 
 <style lang="scss" scoped>
